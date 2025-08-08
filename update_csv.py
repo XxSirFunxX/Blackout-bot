25a56e8df5569ef444717241276a06cbe5ed565b
XxSirFunxX <sirfun.goozdaram@gmail.com>
2025-08-08 23:50:11 +0330

    Delete update_csv.py

--- update_csv.py
+++
@@ -1,9 +0,0 @@
-from fetch_and_save import scrape, save_csv
-
-if __name__ == "__main__":
-    data = scrape()
-    if data:
-        save_csv(data)
-        print("CSV با موفقیت به‌روزرسانی شد.")
-    else:
-        print("داده‌ای دریافت نشد.")
